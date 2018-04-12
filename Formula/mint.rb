990b6a710f4b99a2eccd0bab116794e6905ea594
commitay <commitay@users.noreply.github.com>
2018-04-13 08:46:26 +1000

    Update mint.rb

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,7 +1,7 @@
 class Mint < Formula
   desc "Dependency manager that installs and runs Swift command-line tool packages"
   homepage "https://github.com/yonaskolb/Mint"
-  url "https://github.com/yonaskolb/mint/archive/0.9.0.tar.gz"
+  url "https://github.com/yonaskolb/Mint/archive/0.9.0.tar.gz"
   sha256 "25d3dbbc3ae331d5c0d65d370457c69449dc043298ae4eea76fcf66d89774407"
 
   bottle do
